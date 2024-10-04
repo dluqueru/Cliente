cf95a66b576831e90e6b87270a09faf044dd2b70
Daniel <dluqueru@gmail.com>
2024-10-04 12:34:10 +0200

    componentes de tarjeta

---
+++ pruebaReact/my-first-react-app/src/About.jsx
@@ -0,0 +1,12 @@
+import './Profile.css'
+
+function About(){
+
+    return(
+        <>
+        <h1 className='about'>About us</h1>
+        </>
+    );
+}
+
+export default About
---
+++ pruebaReact/my-first-react-app/src/Profile.jsx
@@ -0,0 +1,26 @@
+import './Profile.css'
+
+const name = "Daniel Luque";
+const age = 29;
+const email = "dluqueru@gmail.com";
+
+function Profile() {
+
+  return (
+    <div className='borde'>
+      <img src="src/img/800px-Sandro_Botticelli_070.jpg" alt="fotillo" />
+      <h1 id='nombre' className='texto'>{name}</h1>
+      <p className='texto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius cum maiores veritatis non animi pariatur quasi nam tempore vitae, aperiam architecto aspernatur, quidem nostrum voluptatibus officiis eaque similique, expedita quaerat qui ipsa nihil odio! Eveniet voluptatem culpa accusamus expedita quaerat veritatis molestias ratione dolorum quidem, blanditiis quae magni delectus officia!</p>
+      <h3>Edad: {age}</h3>
+      <h3>Correo: {email}</h3>
+      <ul>
+        <li>Javascript</li>
+        <li>Php</li>
+        <li>Python</li>
+        <li>Java</li>
+      </ul>
+    </ div>
+  )
+}
+
+export default Profile
