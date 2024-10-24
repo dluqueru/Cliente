f4f255b3afe2958e0352e0a49f984642af96f5c0
Daniel <dluqueru@gmail.com>
2024-10-24 09:45:39 +0200

    terminado boletin eventos

--- DOM/eventos/boletin1.js
+++ DOM/eventos/boletin1.js
@@ -33,11 +33,11 @@ const br = document.createElement('br');
 // 2.1 Agrega un escuchador de eventos al elemento document.body para que cuando se 
 // presione cualquier tecla, muestre un mensaje de alerta con el nombre de la tecla 
 // pulsada y su código.
-document.body.addEventListener('keydown', function(e) {
-    if(e.key == " "){
-        alert(`Tecla pulsada: BARRA ESPACIADORA, código: ${e.code}`)
-    } else {
-        alert(`Tecla pulsada: ${e.key}, código: ${e.code}`)}})
+// document.body.addEventListener('keydown', function(e) {
+//     if(e.key == " "){
+//         alert(`Tecla pulsada: BARRA ESPACIADORA, código: ${e.code}`)
+//     } else {
+//         alert(`Tecla pulsada: ${e.key}, código: ${e.code}`)}})
 
 // 2.2 Prueba el escuchador de eventos con diferentes teclas para verificar que muestra 
 // correctamente el nombre y el código de la tecla pulsada.
@@ -55,7 +55,71 @@ lista.addEventListener('click', (e) => console.log(e.target.innerHTML));
 // nivel de jerarquía en el DOM del elemento clickeado. Por ejemplo, si se hace clic en 
 // un <li>, el mensaje podría decir "Clickeaste en un elemento de nivel 2".
 lista.addEventListener('click', function(e) { 
-    if(e.target.innerHTML != 'Pan'){
-        console.log(e.target.innerHTML)}
+    if(e.target.hasChildNodes()) {
+        console.log("Clickeaste un elemento de nivel 1");
+    } else {
+        console.log("Clickeaste un elemento de nivel 2");
     }
+}
 );
+
+
+/* Ejercicio 4: Eventos de formulario
+4.1. Crea un formulario HTML con campos de entrada para el nombre y el correo electrónico. 
+Agrega un botón "Enviar" al formulario. Cuando se haga clic en el botón, valida que los 
+campos de entrada no estén vacíos y muestra un mensaje de éxito o error en la página en 
+función de la validación. */
+const form = document.querySelector('form');
+const inputNombre = document.getElementById('nombre');
+const inputEmail = document.getElementById('email');
+const botonEnviar = document.getElementById('enviar');
+
+function validaFormulario(){
+    let mensaje = document.createElement('h2');
+
+    if(inputNombre.value == "" || inputEmail.value == ""){
+        mensaje.appendChild(document.createTextNode("Algún campo de entrada está vacío"));
+        mensaje.classList.add('error');
+    } else {
+        mensaje.appendChild(document.createTextNode("Formulario completado correctamente"));
+        mensaje.classList.add('exito');
+        const eventoEnviado = new Event('formularioEnviado');
+        form.dispatchEvent(eventoEnviado);
+    }
+
+    const mensajeAntiguo = document.querySelector('h2');
+
+    if(mensajeAntiguo){
+        mensajeAntiguo.replaceWith(mensaje)
+    } else {
+        form.after(mensaje);
+    }
+}
+
+botonEnviar.addEventListener('click', validaFormulario);
+
+// 4.2. Agrega una función que dispare un evento personalizado "formularioEnviado" 
+// cuando se envíe el formulario con éxito. Escucha este evento y muestra un mensaje 
+// personalizado en la consola cuando se dispare. 
+form.addEventListener('formularioEnviado', function() {
+    console.log("¡El formulario ha sido enviado con éxito!");
+});
+
+
+/* Ejercicio 5: Eventos personalizados */
+// 5.1. Crea un nuevo evento personalizado llamado "miEvento" utilizando new CustomEvent().
+const miEvento = new  CustomEvent('miEvento');
+
+// 5.2. Agrega un escuchador de eventos a un elemento HTML (puedes usar un botón) para 
+// escuchar el evento "miEvento" y mostrar un mensaje de alerta cuando ocurra.
+const botonEvento = document.getElementById('botonEvento');
+
+botonEvento.addEventListener('miEvento', function() {
+    alert('Ocurrió un evento personalizado');
+});
+
+// 5.3. Dispara manualmente el evento "miEvento" en el elemento y verifica que se muestra el 
+// mensaje de alerta.
+botonEvento.addEventListener('click', function() {
+    botonEvento.dispatchEvent(miEvento);
+});
