5fb48f9ce07d19d33ff623bd4578070b00a3f61b
Daniel <dluqueru@gmail.com>
2024-11-08 12:24:08 +0100

    arreglo estilos de lista de skills proyecto react

--- pruebaReact/my-first-react-app/src/Profile.jsx
+++ pruebaReact/my-first-react-app/src/Profile.jsx
@@ -9,12 +9,15 @@ function Profile({name = 'John Doe', age = 100, email = 'example@email.com', ima
       <p className='texto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius cum maiores veritatis non animi pariatur quasi nam tempore vitae, aperiam architecto aspernatur, quidem nostrum voluptatibus officiis eaque similique, expedita quaerat qui ipsa nihil odio! Eveniet voluptatem culpa accusamus expedita quaerat veritatis molestias ratione dolorum quidem, blanditiis quae magni delectus officia!</p>
       <h3>Edad: {age}</h3>
       <h3>Correo: {email}</h3>
-      <ul>
-        <li>Javascript</li>
-        <li>Php</li>
-        <li>Python</li>
-        <li>Java</li>
-      </ul>
+      <div className="container">
+        <ul>
+          <li>Javascript</li>
+          <li>Php</li>
+          <li>Python</li>
+          <li>Java</li>
+          <li>CSS</li>
+        </ul>
+      </div>
     </ div>
   )
 }
